944484223203654a468e6c298f85525463abbc4a
Hans Tool <hans@defect.org>
2021-10-03 09:43:55 +0200

    Missing commas

--- b_and_g/130824.js
+++ b_and_g/130824.js
@@ -9,76 +9,76 @@ module.exports = [
   {
     source: 'DR Bearing',
     node: 'navigation.'
-  }
+  },
   {
     source: 'DR Distance',
     node: 'navigation.'
-  }
+  },
   */
   {
     source: 'Heading on opposite Tack (True)',
     node: 'performance.tackTrue'
-  }
+  },
   {
     source: 'Leeway Angle',
     node: 'navigation.leewayAngle'
-  }
+  },
   {
     source: 'Optimum Wind Angle',
     node: 'performance.beatAngle'
-  }
+  },
   {
     source: 'Polar boat speed',
     node: 'performance.polarSpeed'
-  }
+  },
   {
     source: 'Target TWA',
     node: 'performance.targetAngle'
-  }
+  },
   {
     source: 'Trip time',
     node: 'navigation.trip.lastReset'
-  }
+  },
   /*
   {
     source: 'VMG performance',
     node: ''
-  }
+  },
   */
   {
     source: 'Velocity Made Good',
     node: 'performance.velocityMadeGood'
-  }
+  },
   {
     source: 'Wind Angle to mast',
     node: 'navigation.log'
-  }
+  },
+  {
     source: 'Polar performance',
     node: 'performance.polarSpeedRatio'
-  }
+  },
   /*
   {
     source: 'Opposite tack COG',
     node: 'navigation.'
-  }
+  },
   */
   {
     source: 'Opposite tack target heading',
     node: 'performance.tackTrue'
-  }
+  },
   /*
   {
     source: 'Average True Wind Direction',
     node: 'navigation.log'
-  }
+  },
   {
     source: 'Wind Phase',
     node: 'navigation.log'
-  }
+  },
   {
     source: 'Wind Lift',
     node: 'navigation.log'
   }
   */
 ]
-
